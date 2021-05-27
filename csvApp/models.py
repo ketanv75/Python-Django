2958a68cfc4ce7e8e9f3312809433ee765b84e83
Ketan Vaidya <ketan.vaidya@gmail.com>
2021-05-27 21:14:40 +0200

    Project Commit

---
+++ csvApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class dataFields(models.Model):
+
+    y_field             = models.CharField(max_length=200, blank=True)
+    modelType           = models.CharField(max_length=20, blank=True)
+    modelName           = models.CharField(max_length=20, blank=True)
+    objects             = models.Manager()
+
+    def __str__(self):
+        return self.y_field
